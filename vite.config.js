6d6d2b0b06a5a5a9a99141c584e306bb8d596076
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2025-08-28 10:45:44 +0800

    use cdn for selected deps

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,17 @@ import { updateVendorCache } from './build/vendor';
 import versionUpdatePlugin from './src/utils/versionUpdatePlugin';
 import importToCDN from 'vite-plugin-cdn-import';
 
+const cdnModules = [
+  { name: 'axios', var: 'axios', path: 'https://cdn.staticfile.org/axios/0.21.1/axios.min.js' },
+  { name: 'dayjs', var: 'dayjs', path: 'https://cdn.staticfile.org/dayjs/1.11.13/dayjs.min.js' },
+  { name: 'crypto-js', var: 'CryptoJS', path: 'https://cdn.staticfile.org/crypto-js/4.1.1/crypto-js.min.js' },
+  { name: 'js-cookie', var: 'Cookies', path: 'https://cdn.staticfile.org/js-cookie/3.0.0/js.cookie.min.js' },
+  { name: 'bignumber.js', var: 'BigNumber', path: 'https://cdn.staticfile.org/bignumber.js/9.1.2/bignumber.min.js' },
+  { name: 'nprogress', var: 'NProgress', path: 'https://cdn.staticfile.org/nprogress/0.2.0/nprogress.js' },
+  { name: 'echarts', var: 'echarts', path: 'https://cdn.staticfile.net/echarts/5.4.3/echarts.common.min.js' },
+  { name: 'codemirror', var: 'CodeMirror', path: 'https://cdn.staticfile.org/codemirror/5.65.18/codemirror.js' },
+];
+
 export default ({ mode, command }) => {
   const env = loadEnv(mode, process.cwd());
   const { VITE_APP_BASE_URL, VITE_WEIGHT, VITE_APP_API, VITE_APP_ENV, VITE_APP_BASE } = env;
@@ -17,58 +28,13 @@ export default ({ mode, command }) => {
   const isBuild = command === 'build';
   const currentTimeVersion = Date.now();
 
-  const plugins = [createVitePlugins(env, isBuild)];
+  const plugins = [
+    createVitePlugins(env, isBuild),
+    importToCDN({ injectTo: 'head', modules: cdnModules })
+  ];
 
   if (isBuild && !isLocal) {
-    plugins.push(
-      versionUpdatePlugin({ version: currentTimeVersion }),
-      importToCDN({
-        injectTo: 'head',
-        modules: [
-          // 以下依旧用 UMD，就无需改（它们不会被 import 成命名导出）
-          {
-            name: 'axios',
-            var: 'axios',
-            path: 'https://cdn.staticfile.org/axios/0.21.1/axios.min.js',
-          },
-          {
-            name: 'dayjs',
-            var: 'dayjs',
-            path: 'https://cdn.staticfile.org/dayjs/1.11.13/dayjs.min.js',
-          },
-          {
-            name: 'crypto-js',
-            var: 'CryptoJS',
-            path: 'https://cdn.staticfile.org/crypto-js/4.1.1/crypto-js.min.js',
-          },
-          {
-            name: 'js-cookie',
-            var: 'Cookies',
-            path: 'https://cdn.staticfile.org/js-cookie/3.0.0/js.cookie.min.js',
-          },
-          {
-            name: 'bignumber.js',
-            var: 'BigNumber',
-            path: 'https://cdn.staticfile.org/bignumber.js/9.1.2/bignumber.min.js',
-          },
-          {
-            name: 'nprogress',
-            var: 'NProgress',
-            path: 'https://cdn.staticfile.org/nprogress/0.2.0/nprogress.js',
-          },
-          {
-            name: 'echarts',
-            var: 'echarts',
-            path: 'https://cdn.staticfile.net/echarts/5.4.3/echarts.common.min.js',
-          },
-          {
-            name: 'codemirror',
-            var: 'CodeMirror',
-            path: 'https://cdn.staticfile.org/codemirror/5.65.18/codemirror.js',
-          },
-        ],
-      })
-    );
+    plugins.push(versionUpdatePlugin({ version: currentTimeVersion }));
   }
 
   const proxyObj = {
@@ -126,18 +92,16 @@ export default ({ mode, command }) => {
       target: 'esnext',
       minify: isProd ? 'terser' : 'esbuild',
       rollupOptions: {
-        external: isBuild
-          ? [
-              'axios',
-              'dayjs',
-              'crypto-js',
-              'js-cookie',
-              'bignumber.js',
-              'nprogress',
-              'echarts',
-              'codemirror',
-            ]
-          : [],
+        external: [
+          'axios',
+          'dayjs',
+          'crypto-js',
+          'js-cookie',
+          'bignumber.js',
+          'nprogress',
+          'echarts',
+          'codemirror',
+        ],
         output: {
           // 把裸导入重写到 CDN ESM/UMD 地址
           paths: {
@@ -175,6 +139,16 @@ export default ({ mode, command }) => {
       esbuildOptions: {
         target: 'esnext',
       },
+      exclude: [
+        'axios',
+        'dayjs',
+        'crypto-js',
+        'js-cookie',
+        'bignumber.js',
+        'nprogress',
+        'echarts',
+        'codemirror',
+      ],
     },
     css: {
       preprocessorOptions: {
